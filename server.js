51ed849f1c1381d79e2fef0d3f6b11fb9979ec5c
ekuicsd <46596077+ekuicsd@users.noreply.github.com>
2020-07-01 13:00:04 +0530

    deploy on heroku

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/voila'));
-app.use(express.static(path.join(__dirname,'./voila')));
+// app.use(express.static(path.join(__dirname,'./voila')));
 
 app.get('/', function(req,res) {
 // app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/voila/index.html'));
+res.sendFile(path.join(__dirname+'src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
