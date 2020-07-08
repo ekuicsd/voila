14ed441ba2c911b27eeda1817bbfe72f84c9ff02
architjain808 <architjain808@gmail.com>
2020-07-08 13:44:13 +0530

    loading work all and navbar logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+ import { Component } from '@angular/core';
+import {Router , NavigationCancel,NavigationEnd,NavigationError,NavigationStart, Event} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  ShowLoadingIndicator = true;
+ constructor(private loadRouter : Router){
+   this.loadRouter.events.subscribe((routerEvent :Event) =>
+   {
+     if(routerEvent instanceof NavigationStart){
+       this.ShowLoadingIndicator = true;
+     }
+     if(routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError){
+      this.ShowLoadingIndicator = false;
+    }
+   });
+ }
 }
