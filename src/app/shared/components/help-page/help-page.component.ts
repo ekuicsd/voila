9af58730412374bbefedf5c462235cac091425e6
architjain808 <architjain808@gmail.com>
2020-07-09 15:25:20 +0530

    archit help me

--- src/app/shared/components/help-page/help-page.component.ts
+++ src/app/shared/components/help-page/help-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-help-page',
   templateUrl: './help-page.component.html',
   styleUrls: ['./help-page.component.scss']
 })
 export class HelpPageComponent implements OnInit {
+ addClass() {
+      document.body.classList.add("sent");
+    }
 
   constructor() { }
 
   ngOnInit() {
+
+
   }
 
 }
