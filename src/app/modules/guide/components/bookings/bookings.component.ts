f377c685799ac69ffaed152657b39cb07912209c
ekuicsd <46596077+ekuicsd@users.noreply.github.com>
2020-07-11 14:55:34 +0530

    bookingspage half

--- src/app/modules/guide/components/bookings/bookings.component.ts
+++ src/app/modules/guide/components/bookings/bookings.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { GuideService } from 'src/app/shared/service/guide.service';
 import { Booking } from 'src/app/shared/models/booking.model';
 import { Router } from '@angular/router';
-
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
@@ -10,9 +10,12 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None
 })
 export class BookingsComponent implements OnInit {
-  public bookingsList: Booking[];
+  public bookingsList: Booking[] = [];
+  public selectedBooking: Booking;
 
-  constructor(private guideService: GuideService, private router: Router) { }
+  constructor(private guideService: GuideService,
+    config: NgbModalConfig, private modalService: NgbModal,
+     private router: Router) { }
 
   ngOnInit() {
     this.getAllBookings();
@@ -21,7 +24,11 @@ export class BookingsComponent implements OnInit {
   getAllBookings() {
     this.guideService.getAllBookingsByStatus('APPROVED').subscribe(
       res => {
-        this.bookingsList = res;
+        if(res.length > 0) {
+          this.bookingsList = res;
+        } else {
+          this.bookingsList = undefined;
+        }
         console.log(this.bookingsList);
       }, error => {
         console.log(error);
@@ -29,25 +36,14 @@ export class BookingsComponent implements OnInit {
     )
   }
 
-  // getDuration(startDate: Date, endDate: Date): string {
-  //   console.log(startDate);
-  //   console.log(startDate.getDate());
-  //   console.log(startDate.getMonth());
-  //   if(endDate.getMonth() - startDate.getMonth()){
-
-
-  //   }else {
-  //     let dayDiff = endDate.getDate() - startDate.getDate();
-  //     // if(dayDiff) {
-  //       return (dayDiff+1) + ' days';
-  //     // }
-  // }
-  //   return '0';
-  // }
-
   contactTourist(email) {
     console.log(email);
-    //navigate too their chatbox
     this.router.navigateByUrl('/guide/guidehome/chats/tourist/' + email);
+  }
+
+  open(content, data) {
+    console.log(data);
+    this.selectedBooking = data;
+    this.modalService.open(content, { centered: true, scrollable: true});
   }
 }
