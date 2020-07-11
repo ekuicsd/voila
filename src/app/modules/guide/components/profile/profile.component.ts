9efbe7bf95e0a3647bdced0cacae968a8170e9a4
ekuicsd <46596077+ekuicsd@users.noreply.github.com>
2020-07-12 00:54:30 +0530

    guide profile

--- src/app/modules/guide/components/profile/profile.component.ts
+++ src/app/modules/guide/components/profile/profile.component.ts
@@ -6,7 +6,10 @@ import { MDBModalService } from 'angular-bootstrap-md';
 import { GuideService } from 'src/app/shared/service/guide.service';
 import { ToastrService } from 'ngx-toastr';
 import { StaticDataService } from 'src/app/shared/service/static-data.service';
-// import csc from 'country-state-city';
+import csc from 'country-state-city';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import languages from 'country-language';
+import { CustomValidators } from 'src/app/validators/custom';
 
 @Component({
   selector: 'app-profile',
@@ -25,12 +28,14 @@ export class ProfileComponent implements OnInit {
   public userData: Guide;
   public interestList: string[];
   public selectedExp: Experience;
-  // public stateList: any[];
-  // public cityList: any[]; 
-  // public selectedStateId: any; 
+  public languageList: any[] = [];
+  public expVariable: string = '';
+  public stateList: any[];
+  public cityList: any[]; 
 
   constructor(private userService: UserService, 
     private guideService: GuideService,
+    config: NgbModalConfig, private modalService: NgbModal,
     private staticDataService: StaticDataService,
     private toastr: ToastrService
     ) {  }
@@ -43,13 +48,15 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {  
     this.getUser();
-    // this.getAllState('101');
+    this.getAllState('101');
     this.createPersonalDetailsForm();
     this.createBusinessDetails();
     this.createExperienceDetailsForm();
     this.createLanguagesForm();
     this.createInterestsForm();
     this.interestList = this.staticDataService.getAllInterestList();
+    this.languageList = languages.getLanguages().map(ele => ele.name[0]);
+
   }
 
   getUser() {
@@ -57,34 +64,40 @@ export class ProfileComponent implements OnInit {
     console.log(this.userData);
   }
 
+  convertDateToString(strdate) : string {
+    let date = new Date(strdate);
+    return date.toJSON().slice(0, 10);
+  }
+
   createPersonalDetailsForm() {
+    this.getAllCity(this.userData.state);
     this.personalDetails = new FormGroup({
-      name: new FormControl(this.userData.name, [Validators.required]),
+      name: new FormControl(this.userData.name, [Validators.required, Validators.minLength(3)]),
       gender: new FormControl(this.userData.gender, [Validators.required]),
-      dob: new FormControl(this.userData.dob, [Validators.required]),
-      email: new FormControl(this.userData.email, [Validators.required]),
-      aadhaarNumber: new FormControl(this.userData.aadhaarNumber, [Validators.required]),
-      phoneNumber: new FormControl(this.userData.phoneNumber, [Validators.required]),
+      dob: new FormControl(this.convertDateToString(this.userData.dob), [Validators.required]),
+      email: new FormControl(this.userData.email, [Validators.required, Validators.email]),
+      aadhaarNumber: new FormControl(this.userData.aadhaarNumber, [Validators.required, CustomValidators.compondValueValidate]),
+      phoneNumber: new FormControl(this.userData.phoneNumber, [Validators.required, CustomValidators.contactNumber]),
       state: new FormControl(this.userData.state, [Validators.required]),
       city: new FormControl(this.userData.city, [Validators.required]),
-      address: new FormControl(this.userData.address, [Validators.required])
-    })
+      address: new FormControl(this.userData.address, [Validators.required, Validators.minLength(3)])
+    });
   }
 
   createBusinessDetails() {
     this.businessDetails = new FormGroup({
-      peopleLimit: new FormControl(this.userData.peopleLimit, [Validators.required]),
-      perHeadCharge: new FormControl(this.userData.perHeadCharge, [Validators.required]),
-      perDayCharge: new FormControl(this.userData.perDayCharge, [Validators.required])
+      peopleLimit: new FormControl(this.userData.peopleLimit, [Validators.required, CustomValidators.compondValueValidate]),
+      perHeadCharge: new FormControl(this.userData.perHeadCharge, [Validators.required, CustomValidators.compondValueValidate]),
+      perDayCharge: new FormControl(this.userData.perDayCharge, [Validators.required, CustomValidators.compondValueValidate])
     })
   }
 
   createExperienceDetailsForm() {
     this.experienceDetails = new FormGroup({
-      work: new FormControl('', [Validators.required]),
-      startYear: new FormControl('', [Validators.required]),
-      duration: new FormControl('', [Validators.required]),
-      profile: new FormControl('', [Validators.required])
+      work: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      startYear: new FormControl('', [Validators.required, CustomValidators.compondValueValidate, Validators.minLength(4), Validators.maxLength(4)]),
+      duration: new FormControl('', [Validators.required, CustomValidators.compondValueValidate]),
+      profile: new FormControl('', [Validators.required, Validators.minLength(3)])
     })
   }
 
@@ -100,8 +113,10 @@ export class ProfileComponent implements OnInit {
     })
   }
 
+
   editExpShow(content, data: Experience) {
     this.editExp = true;
+    this.expVariable = 'Update Experience';
     this.selectedExp = data;
     console.log(this.selectedExp);
     this.experienceDetails.patchValue(data);
@@ -110,6 +125,7 @@ export class ProfileComponent implements OnInit {
 
   addExpShow(content) {
     this.editExp = false;
+    this.expVariable = 'Add Experience';
     this.createExperienceDetailsForm();
     this.modal.show(content);
   }
@@ -122,13 +138,7 @@ export class ProfileComponent implements OnInit {
       this.userData.experience[index].startYear = this.experienceDetails.value.startYear;
       this.userData.experience[index].work = this.experienceDetails.value.work;
       console.log(this.userData);
-      this.guideService.updateUserDetails(this.userData).subscribe( res => {
-        console.log(res);
-        this.userService.saveUser(res.profile, 'guide');
-        this.getUser();
-        this.modal.hide(0);
-        this.toastr.success("Experience Updated Successfully!");
-      });
+      this.modal.hide(0);
     } else {
       this.toastr.error("Invalid Details!");
     }
@@ -137,48 +147,35 @@ export class ProfileComponent implements OnInit {
   removeExperience(item) {
     let index = this.userData.experience.indexOf(item);
     this.userData.experience.splice(index, 1);
-    this.guideService.updateUserDetails(this.userData).subscribe( res => {
-      console.log(res);
-      this.userService.saveUser(res.profile, 'guide');
-      this.getUser();
-      this.toastr.success("Experience Removed Successfully!");
-    });
   }
 
-  // getAllState(countryId: string) {
-  //   console.log(countryId);
-  //   this.stateList = csc.getStatesOfCountry(countryId);
-  //   console.log(this.stateList);
-  // }
+  getAllState(countryId: string) {
+    console.log(countryId);
+    this.stateList = csc.getStatesOfCountry(countryId);
+    console.log(this.stateList);
+  }
 
-  // getAllCity(stateId) {
-  //   this.cityList = csc.getCitiesOfState(stateId);
-  //   console.log(this.cityList);
-  // }
+  getAllCity(statename) {
+    let stateId = this.stateList.filter(ele => {
+      if(ele.name === statename) {
+        return ele;
+      }
+    })[0].id;
+    console.log(stateId);
+    this.cityList = csc.getCitiesOfState(stateId);
+    console.log(this.cityList);
+  }
 
 
   submitPersonalDetails() {
     if(this.personalDetails.valid) {
-      //getStateName
-      // let selectedState = csc.getStateById(this.personalDetails.value.state);
-      // console.log(selectedState);
-      // this.personalDetails.patchValue({
-      //   state: selectedState.name
-      // })
-      //----------------------
       this.userData.gender = this.personalDetails.value.gender;
       this.userData.name = this.personalDetails.value.name;
       this.userData.address = this.personalDetails.value.address;
       this.userData.city = this.personalDetails.value.city;
       this.userData.state = this.personalDetails.value.state;
       this.userData.phoneNumber = this.personalDetails.value.phoneNumber;
-      this.guideService.updateUserDetails(this.userData).subscribe( res => {
-        console.log(res);
-        this.userService.saveUser(res.profile, 'guide');
-        this.getUser();
         this.modal1.hide(0);
-        this.toastr.success("Personal Details Updated Successfully!");
-      });
     } else {
       this.toastr.error("Invalid Details!");
     }
@@ -189,13 +186,7 @@ export class ProfileComponent implements OnInit {
       this.userData.peopleLimit = this.businessDetails.value.peopleLimit;
       this.userData.perDayCharge = this.businessDetails.value.perDayCharge;
       this.userData.perHeadCharge = this.businessDetails.value.perHeadCharge;
-      this.guideService.updateUserDetails(this.userData).subscribe( res => {
-        console.log(res);
-        this.userService.saveUser(res.profile, 'guide');
-        this.getUser();
         this.modal4.hide(0);
-        this.toastr.success("Business Details Updated Successfully!");
-      });
     } else {
       this.toastr.error("Invalid Details!");
     }
@@ -204,13 +195,7 @@ export class ProfileComponent implements OnInit {
   addExperience() {
     if(this.experienceDetails.valid) {
       this.userData.experience.push(this.experienceDetails.value);
-      this.guideService.updateUserDetails(this.userData).subscribe( res => {
-        console.log(res);
-        this.userService.saveUser(res.profile, 'guide');
-        this.getUser();
         this.modal.hide(0);
-        this.toastr.success("Experience Added Successfully!");
-      });
     } else {
       this.toastr.error("Invalid Details!");
     }
@@ -218,15 +203,19 @@ export class ProfileComponent implements OnInit {
 
   addLanguage() {
     if(this.languagesDetails.valid) {
-      this.userData.languages.push(this.languagesDetails.value.language);
-      this.guideService.updateUserDetails(this.userData).subscribe( res => {
-        console.log(res);
-        this.userService.saveUser(res.profile, 'guide');
-        this.getUser();
-        this.modal2.hide(0);
-        this.createLanguagesForm();
-        this.toastr.success("Language Added Successfully!");
-      });
+        let language = this.userData.languages.filter( ele => {
+          if(ele === this.languagesDetails.value.language) {
+            return ele;
+          }
+        });
+        console.log(language);
+        if(language.length >= 1) {
+            this.toastr.warning("Already Added!"); 
+            this.modal2.hide(0);
+        } else {
+          this.userData.languages.push(this.languagesDetails.value.language);
+          this.modal2.hide(0);
+        }
     } else {
       this.toastr.error("Invalid Details!");
     }
@@ -246,14 +235,7 @@ export class ProfileComponent implements OnInit {
           this.modal3.hide(0);
       } else {
         this.userData.interests.push(this.interestsDetails.value.interest);
-        this.guideService.updateUserDetails(this.userData).subscribe( res => {
-          console.log(res);
-          this.userService.saveUser(res.profile, 'guide');
-          this.getUser();
-          this.modal3.hide(0);
-          this.createInterestsForm();
-          this.toastr.success("Interest Added Successfully!");
-        });
+        this.modal3.hide(0);
       }
     } else {
       this.toastr.error("Invalid Details!");
@@ -264,22 +246,21 @@ export class ProfileComponent implements OnInit {
   removeLanguage(item) {
     let index = this.userData.languages.indexOf(item);
     this.userData.languages.splice(index, 1);
-    this.guideService.updateUserDetails(this.userData).subscribe( res => {
-      console.log(res);
-      this.userService.saveUser(res.profile, 'guide');
-      this.getUser();
-      this.toastr.success("Language Removed Successfully!");
-    });
   }
   
   removeInterests(item) {
     let index = this.userData.interests.indexOf(item);
     this.userData.interests.splice(index, 1);
-    this.guideService.updateUserDetails(this.userData).subscribe( res => {
-      console.log(res);
-      this.userService.saveUser(res.profile, 'guide');
-      this.getUser();
-      this.toastr.success("Interest Removed Successfully!");
-    });
   }
+
+  saveRecord() {
+    this.guideService.updateUserDetails(this.userData).subscribe( res => {
+        console.log(res);
+        this.userService.saveUser(res.profile, 'guide');
+        this.getUser();
+        this.toastr.success("Profile Updated Successfully!");
+      });
+  }
+
+
 }
