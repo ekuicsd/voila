b026a52563f700d7b3cf565aff9bb01f215e43c6
ekuicsd <46596077+ekuicsd@users.noreply.github.com>
2020-07-23 19:38:27 +0530

    position of header get fixed

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,7 +4,10 @@ import { UserService } from '../../service/user.service';
 import { MessageService } from '../../service/message.service';
 import { JwtService } from '../../service/jwt.service';
 import { Location } from '@angular/common';
-import { NavbarComponent } from 'angular-bootstrap-md';
+import { NavbarComponent, MDBModalService } from 'angular-bootstrap-md';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GuideService } from '../../service/guide.service';
+import { TouristsService } from '../../service/tourists.service';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +16,14 @@ import { NavbarComponent } from 'angular-bootstrap-md';
 })
 export class HeaderComponent implements OnInit{
 
+  @ViewChild('basicModal', { static: false}) modal1: MDBModalService;
   @ViewChild('navbarid', {static: false}) navbaridRef: NavbarComponent;
   @Input() loggingIn;
   msgNumber = 0;
   public user: any;
   public recieverRole;
   public userRole;
+  public onGoingEvents: any[] = [];
   public chatList = [];
   
   constructor(
@@ -26,6 +31,9 @@ export class HeaderComponent implements OnInit{
     public jwtService: JwtService,
     private msgService: MessageService,
     private location: Location,
+    private modalService: NgbModal,
+    private guideService: GuideService,
+    private touristService: TouristsService,
     public userService: UserService) {
     }
 
@@ -42,5 +50,47 @@ export class HeaderComponent implements OnInit{
   logout(role: string) {
       this.userService.logout(role);
   }
+
+  getOngoingEvents(basicModal) {
+    if(this.userService.getRole() === 'guide') {
+     this.guideService.getAllBookingsByStatus('ONGOING').subscribe( res => {
+       this.onGoingEvents = res;
+       if(res.length <= 0) {
+         this.onGoingEvents = undefined;
+       }
+       this.modal1.show(basicModal);
+       console.log(res);
+     });
+    } else {
+     this.touristService.getAllBookingsByStatus('ONGOING').subscribe( res => {
+       this.onGoingEvents = res;
+       if(res.length <= 0) {
+         this.onGoingEvents = undefined;
+       }
+       console.log(res);
+       this.modal1.show(basicModal);
+     });
+    }
+  }
+ 
+  openModal(content) {
+   this.modalService.open(content);
+  }
+ 
+  open(content) {
+   this.modalService.open(content);
+   }
+ 
+   getEndDate(data) {
+     if(data) {
+       let today = new Date();
+       let end = new Date(data.endDate);
+       let diff = (end.getTime() - today.getTime()) / (1000 * 3600 * 24);
+       if(diff >= 0) {
+         return true;
+       }
+     }
+     return false;
+   }
 
 }
